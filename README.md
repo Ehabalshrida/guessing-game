e8da1a4a27317921fc97cca537d4bc6945da5ba3
ehabalshrida <ehabshridah@gmail.com>
2021-07-27 18:46:38 +0300

    update the files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # guessing-game
+
+## about me project 
+
+*in this project i have added Preif about my self, and about my education Background, my experience, and added
+an ineractive interface with visitor to get some input info frome his side*
--- js/app.js
+++ js/app.js
@@ -1,77 +1,125 @@
 'use strict';
+let finalScore = 0;
 let userName = prompt ('what is your name ');
 alert('nice to meet you '+ userName);
 
 let known = prompt ('Do we know me ?' , 'your answer should be in lowercase');
 switch(known.toLowerCase()) {
-    case 'yes':
-      alert ('So happy to hear that');
-     // console.log(known);
-      break;
-    case 'no':
-      alert('I hope we get the opportunity to meet you ');
-     // console.log(known);
-      break;
-    default:
-      alert('You didnt answer');
+case 'yes':
+  alert ('So happy to hear that');
+  finalScore++;
+  // console.log(known);
+  break;
+case 'no':
+  alert('I hope we get the opportunity to meet you ');
+  // console.log(known);
+  break;
+default:
+  alert('You didnt answer');
       //console.log(known);
-  }
+}
 
-  let programer = prompt ('are you programmer?' , 'your answer should be in lowercase' );
+let programer = prompt ('are you programmer?' , 'your answer should be in lowercase' );
 switch(programer.toLowerCase()) {
-    case 'yes':
-      alert ('So we are  colleagues');
-     // console.log(programer);
-      break;
-    case 'no':
-      alert('i hope you will be best programmer in the future');
-     // console.log(programer);
-      break;
-    default:
-      alert('You didnt answer ');
+case 'yes':
+  alert ('So we are  colleagues');
+  finalScore++;
+  // console.log(programer);
+  break;
+case 'no':
+  alert('i hope you will be best programmer in the future');
+  // console.log(programer);
+  break;
+default:
+  alert('You didnt answer ');
       //console.log(programer);
-  }
-  let fan = prompt ('Do you like football ?', 'your answer should be in UpperCase');
-  switch(fan.toUpperCase()) {
-      case 'YES':
-        alert ('i like it also');
-       // console.log(fan);
-        break;
-      case 'NO':
-        alert('you have to watch one match to change your opinion');
-       // console.log(fan);
-        break;
-      default:
-        alert('You didnt answer ');
+}
+let fan = prompt ('Do you like football ?', 'your answer should be in UpperCase');
+switch(fan.toUpperCase()) {
+case 'YES':
+  alert ('i like it also');
+  finalScore++;
+  // console.log(fan);
+  break;
+case 'NO':
+  alert('you have to watch one match to change your opinion');
+  // console.log(fan);
+  break;
+default:
+  alert('You didnt answer ');
         //console.log(fan);
-    }
-    let web = prompt ('Do you like my web page ?', 'your answer should be in UpperCase');
-    switch(web.toUpperCase()) {
-        case 'YES':
-          alert ('Thanks you ');
-         // console.log(web);
-          break;
-        case 'NO':
-          alert('your comments will help me to do the best');
-         // console.log(web);
-          break;
-        default:
-          alert('You didnt answer');
+}
+let web = prompt ('Do you like my web page ?', 'your answer should be in UpperCase');
+switch(web.toUpperCase()) {
+case 'YES':
+  alert ('Thanks you ');
+  finalScore++;
+  // console.log(web);
+  break;
+case 'NO':
+  alert('your comments will help me to do the best');
+  // console.log(web);
+  break;
+default:
+  alert('You didnt answer');
           //console.log(web);
-      }
-      let stay = prompt ('Do you like to stay in contact ? ', 'your answer should be in UpperCase');
-    switch(stay.toUpperCase()) {
-        case 'YES':
-          alert ('Thanks i am so happy to hear that ');
-         // console.log(stay);
-          break;
-        case 'NO':
-          alert('I Respect your decision   :) ');
-         // console.log(stay);
-          break;
-        default:
-          alert('You didnt answer');
+}
+let stay = prompt ('Do you like to stay in contact ? ', 'your answer should be in UpperCase');
+switch(stay.toUpperCase()) {
+case 'YES':
+  alert ('Thanks i am so happy to hear that ');
+  finalScore++;
+  // console.log(stay);
+  break;
+case 'NO':
+  alert('I Respect your decision   :) ');
+  // console.log(stay);
+  break;
+default:
+  alert('You didnt answer');
           //console.log(stay);
-      }
+}
 
-      alert('Thanks for your answers  '+ UserName);
+alert('Thanks for your answers  '+ userName);
+
+let weight = Number(prompt('How much do you think my weight')) ;
+let i;
+
+for(i = 0 ; i < 4 ; i++ ){
+  if(weight === 70)
+  {
+    finalScore++;
+    break;
+
+  }
+  if(weight > 70)
+  {
+    alert('your answer is to high');
+    weight = Number(prompt('How much do you think my weight'));
+  }
+  else(weight < 70);
+  {
+    alert('your answer is to low');
+    weight = Number(prompt('How much do you think my weight'));
+  }
+}
+
+alert('the correct answer is:70');
+
+
+let guess = Number(prompt('guess one of three values,my day of birth,last 3 numbers of my cell phone ,my graduation year'));
+
+let listOfdata = [515 , 2011 , 24 ];
+let n;
+let j;
+for (n = 0 ; n < 6 ; n++){
+  for (j = 0 ; j < 3 ; j++ ) {
+    if (guess === listOfdata[j]){
+      alert('you have insert correct answer');
+      finalScore++;
+      break;}
+  }
+  guess = Number(prompt('guess one of three values,my day of birth,last 3 numbers of my cell phone ,my graduation year'));
+}
+alert('my last 3 numbers of my cell phone: ' + listOfdata[0] + ' my graduation year: '+ listOfdata[1] + 'my day of birth: ' + listOfdata[2]);
+alert('your final score is: ' + finalScore + 'of 7');
