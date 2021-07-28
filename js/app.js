4f4ae094d554a9e5931489e48e3cee34b6ef294d
ehabalshrida <ehabshridah@gmail.com>
2021-07-28 14:38:50 +0300

    updated the files

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ alert('nice to meet you '+ userName);
 let known = prompt ('Do we know me ?' , 'your answer should be in lowercase');
 switch(known.toLowerCase()) {
 case 'yes':
+case 'y':
   alert ('So happy to hear that');
   finalScore++;
   // console.log(known);
   break;
 case 'no':
+case 'n':
   alert('I hope we get the opportunity to meet you ');
   // console.log(known);
   break;
@@ -22,11 +24,13 @@ default:
 let programer = prompt ('are you programmer?' , 'your answer should be in lowercase' );
 switch(programer.toLowerCase()) {
 case 'yes':
+case 'y':
   alert ('So we are  colleagues');
   finalScore++;
   // console.log(programer);
   break;
 case 'no':
+case 'n':
   alert('i hope you will be best programmer in the future');
   // console.log(programer);
   break;
@@ -37,11 +41,13 @@ default:
 let fan = prompt ('Do you like football ?', 'your answer should be in UpperCase');
 switch(fan.toUpperCase()) {
 case 'YES':
+case 'Y':
   alert ('i like it also');
   finalScore++;
   // console.log(fan);
   break;
 case 'NO':
+case 'N':
   alert('you have to watch one match to change your opinion');
   // console.log(fan);
   break;
@@ -52,11 +58,13 @@ default:
 let web = prompt ('Do you like my web page ?', 'your answer should be in UpperCase');
 switch(web.toUpperCase()) {
 case 'YES':
+case 'Y':
   alert ('Thanks you ');
   finalScore++;
   // console.log(web);
   break;
 case 'NO':
+case 'N':
   alert('your comments will help me to do the best');
   // console.log(web);
   break;
@@ -67,11 +75,13 @@ default:
 let stay = prompt ('Do you like to stay in contact ? ', 'your answer should be in UpperCase');
 switch(stay.toUpperCase()) {
 case 'YES':
+case 'Y':
   alert ('Thanks i am so happy to hear that ');
   finalScore++;
   // console.log(stay);
   break;
 case 'NO':
+case 'N':
   alert('I Respect your decision   :) ');
   // console.log(stay);
   break;
@@ -89,6 +99,7 @@ for(i = 0 ; i < 4 ; i++ ){
   if(weight === 70)
   {
     finalScore++;
+    alert('Greate You had guessed the right answer its 70');
     break;
 
   }
@@ -117,7 +128,8 @@ for (n = 0 ; n < 6 ; n++){
     if (guess === listOfdata[j]){
       alert('you have insert correct answer');
       finalScore++;
-      break;}
+      break ;}
+
   }
   guess = Number(prompt('guess one of three values,my day of birth,last 3 numbers of my cell phone ,my graduation year'));
 }
